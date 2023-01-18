826f5e350a0e3d47c3f50f3fcb4c9fbb3385f00d
Nahid Amini <namini@diligent.com>
2023-01-18 15:12:50 -0800

    modification

--- python-Selenium/Config/config.py
+++ python-Selenium/Config/config.py
@@ -2,12 +2,12 @@ class TestData:
     CHROME_EXECUTABLE_PATH = '/opt/homebrew/bin/chromedriver'
 
     BASE_URL = "https://cloud.dwavesys.com/leap/login/"
-    USER_NAME = "na_amini55@yahoo.com"
-    FAKE_USER = "abc@gmail.com"
-    PASSWORD = "Selenium123!"
-    ACCOUNT_HOLDER_NAME = "Nahid Amini"
+    USER_NAME = ""
+    PASSWORD = ""
+    FAKE_USER = "fakeuser@abc.com"
+    ACCOUNT_HOLDER_NAME = ""
     ACCOUNT_TYPE = "Developer Plan"
-    SUBSCRIPTION_RENEWAL = "February 18, 2023 (UTC)"
+    SUBSCRIPTION_RENEWAL = ""
     PROJECT = "D-Wave Testing Production"
     LOGIN_PAGE_TITLE = "D-Wave Leap Log In | D-Wave Leap"
     PAGE_TITLE = "Dashboard | D-Wave Leap"
