e850ccff959858769efff70f01ae0d6df4ff4bab
Nahid Amini <namini@diligent.com>
2023-01-18 15:20:15 -0800

    modification

--- python-Selenium/Tests/conftest.py
+++ python-Selenium/Tests/conftest.py
@@ -5,11 +5,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 
-@pytest.fixture(params= ["chrome"], scope='function' )
+@pytest.fixture(params=["chrome"], scope='function')
 def init_driver(request):
     if request.param == "chrome":
         web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         web_driver.maximize_window()
+
     if request.param == "firefox":
         web_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
         web_driver.maximize_window()
